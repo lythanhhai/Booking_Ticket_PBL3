2e7165be1cccee0e1bc8167c07d9c9cb0f9928ed
241201binhle <you@example.com>
2021-05-09 23:16:21 +0700

    ticket

--- PBL3_DATVEXE/View/infoTicket.Designer.cs
+++ PBL3_DATVEXE/View/infoTicket.Designer.cs
@@ -30,124 +30,182 @@ namespace PBL3_DATVEXE.View
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lb_den = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lb_di = new System.Windows.Forms.Label();
+            this.ib_date = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.lb_total_price = new System.Windows.Forms.Label();
+            this.lb_namevehicle = new System.Windows.Forms.Label();
+            this.lb_time_start = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.lb_numberseat = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(209, 39);
+            this.label1.Location = new System.Drawing.Point(128, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Thông tin vé";
             // 
-            // label2
+            // lb_den
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(154, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lb_den.AutoSize = true;
+            this.lb_den.Location = new System.Drawing.Point(87, 55);
+            this.lb_den.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_den.Name = "lb_den";
+            this.lb_den.Size = new System.Drawing.Size(35, 13);
+            this.lb_den.TabIndex = 1;
+            this.lb_den.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(256, 84);
+            this.label3.Location = new System.Drawing.Point(150, 55);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(13, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "--";
             // 
-            // label4
+            // lb_di
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(213, 142);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.lb_di.AutoSize = true;
+            this.lb_di.Location = new System.Drawing.Point(176, 55);
+            this.lb_di.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_di.Name = "lb_di";
+            this.lb_di.Size = new System.Drawing.Size(35, 13);
+            this.lb_di.TabIndex = 3;
+            this.lb_di.Text = "label4";
             // 
-            // label5
+            // ib_date
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(154, 196);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.ib_date.AutoSize = true;
+            this.ib_date.Location = new System.Drawing.Point(141, 87);
+            this.ib_date.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ib_date.Name = "ib_date";
+            this.ib_date.Size = new System.Drawing.Size(35, 13);
+            this.ib_date.TabIndex = 4;
+            this.ib_date.Text = "label5";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(260, 195);
+            this.label6.Location = new System.Drawing.Point(87, 126);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.Size = new System.Drawing.Size(58, 13);
             this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
+            this.label6.Text = "Tổng tiền :";
             // 
-            // label7
+            // lb_total_price
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(158, 256);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(51, 20);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "label7";
+            this.lb_total_price.AutoSize = true;
+            this.lb_total_price.Location = new System.Drawing.Point(160, 126);
+            this.lb_total_price.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_total_price.Name = "lb_total_price";
+            this.lb_total_price.Size = new System.Drawing.Size(35, 13);
+            this.lb_total_price.TabIndex = 6;
+            this.lb_total_price.Text = "label7";
             // 
-            // label8
+            // lb_namevehicle
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(231, 256);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(51, 20);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "label8";
+            this.lb_namevehicle.AutoSize = true;
+            this.lb_namevehicle.Location = new System.Drawing.Point(17, 179);
+            this.lb_namevehicle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_namevehicle.Name = "lb_namevehicle";
+            this.lb_namevehicle.Size = new System.Drawing.Size(35, 13);
+            this.lb_namevehicle.TabIndex = 7;
+            this.lb_namevehicle.Text = "label8";
             // 
-            // label9
+            // lb_time_start
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(162, 322);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(51, 20);
-            this.label9.TabIndex = 8;
-            this.label9.Text = "label9";
+            this.lb_time_start.AutoSize = true;
+            this.lb_time_start.Location = new System.Drawing.Point(72, 204);
+            this.lb_time_start.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_time_start.Name = "lb_time_start";
+            this.lb_time_start.Size = new System.Drawing.Size(35, 13);
+            this.lb_time_start.TabIndex = 8;
+            this.lb_time_start.Text = "label9";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(231, 322);
+            this.label10.Location = new System.Drawing.Point(17, 204);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(60, 20);
+            this.label10.Size = new System.Drawing.Size(49, 13);
             this.label10.TabIndex = 9;
-            this.label10.Text = "label10";
+            this.label10.Text = "Chuyến :";
+            // 
+            // lb_numberseat
+            // 
+            this.lb_numberseat.AutoSize = true;
+            this.lb_numberseat.Location = new System.Drawing.Point(170, 204);
+            this.lb_numberseat.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_numberseat.Name = "lb_numberseat";
+            this.lb_numberseat.Size = new System.Drawing.Size(41, 13);
+            this.lb_numberseat.TabIndex = 10;
+            this.lb_numberseat.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(215, 204);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 13);
+            this.label12.TabIndex = 11;
+            this.label12.Text = ":Ghế";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(163, 235);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(161, 67);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // infoTicket
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 502);
+            this.ClientSize = new System.Drawing.Size(354, 326);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lb_numberseat);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lb_time_start);
+            this.Controls.Add(this.lb_namevehicle);
+            this.Controls.Add(this.lb_total_price);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ib_date);
+            this.Controls.Add(this.lb_di);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_den);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "infoTicket";
             this.Text = "infoTicket";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,14 +214,18 @@ namespace PBL3_DATVEXE.View
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_den;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lb_di;
+        private System.Windows.Forms.Label ib_date;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lb_total_price;
+        private System.Windows.Forms.Label lb_namevehicle;
+        private System.Windows.Forms.Label lb_time_start;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lb_numberseat;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
